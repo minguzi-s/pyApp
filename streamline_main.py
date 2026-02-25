ae4f4e768099e63846ea3f25171f05c7a23912da
minguzi-s <jiminxshin@gmail.com>
2026-02-25 00:24:48 +0000

    change code

--- streamline_main.py
+++ streamline_main.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title("Hello Streamline")
+st.title("hey")
+st.header("yo")
+st.header("hey yo :suglasses:")
+st.text("text")
+st.caption("caption")
+sample_code = '''
+def function():
+    print('hello, world')
+'''
+
+st.text(sample_code)
