fd65287ae89334003ab7042bac0b02a9b5e3fa89
minguzi-s <jiminxshin@gmail.com>
2026-02-25 01:05:04 +0000

    end

--- section 2.py
+++ section 2.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st 
 import os,time
 import random
 st.title(":sparkles: Lotto")
@@ -10,3 +10,13 @@ button = st.button('create lotto')
 if button:
     for i in range(5):
         st.subheader(f"{i+1}")
+
+def generate_lotto():
+    lotto = [i + 1 for i in range(45)]
+    random.shuffle(lotto)
+    return lotto[:6]
+button = st.button("find lotte number")
+
+if button: 
+    for i in range(5):
+        st.subheader(f"{i+1}. lucky number")
